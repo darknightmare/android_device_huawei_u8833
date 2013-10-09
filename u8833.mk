2fdbccfe41d64bf9e9e734936cc8f2dba6d529e5
darknightmare <rubentkd98@gmail.com>
2013-10-09 23:03:32 +0200

    Update u8833.mk
    PRODUCT_NAME now generic, for no problems when compiling.

--- u8833.mk
+++ u8833.mk
@@ -125,7 +125,7 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := aokp_u8833
+PRODUCT_NAME := u8833
 PRODUCT_DEVICE := u8833
 PRODUCT_MANUFACTURER := HUAWEI
 PRODUCT_MODEL := Ascend Y300
